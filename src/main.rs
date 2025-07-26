e87e8756e549e8cc7fff662acf7c9e2def2a225b
Sophia Bitterstar <sophia@shiningtrapezohedron.com>
2025-07-26 20:39:00 +0100

    Modrinth Project model

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 mod cli;
 mod config;
 mod launchers;
+mod sources;
 mod repl;
 
-use crate::repl::repl;
+// use crate::repl::repl;
+use crate::sources::modrinth::fetch_project;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-  repl()
-    .map_err(|e| {
-      eprintln!("Error: {}", e);
-      e
-    })
-    .and_then(|_| {
-      println!("Exiting REPL.");
-      Ok(())
-    })
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let project = fetch_project("create").await?;
+  println!("{:#?}", project);
+
+  // repl()
+  //   .map_err(|e| {
+  //     eprintln!("Error: {}", e);
+  //     e
+  //   })
+  //   .and_then(|_| {
+  //     println!("Exiting REPL.");
+  //     Ok(())
+  //   })
+
+  Ok(())
 }
---
+++ src/sources/modrinth.rs
@@ -0,0 +1,140 @@
+use reqwest::Client;
+use serde::Deserialize;
+
+const ENDPOINT: &str = "https://api.modrinth.com/v2";
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum ClientSide {
+  Required,
+  Optional,
+  Unsupported,
+  Unknown,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum ServerSide {
+  Required,
+  Optional,
+  Unsupported,
+  Unknown,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Status {
+  Approved,
+  Archived,
+  Rejected,
+  Draft,
+  Unlisted,
+  Processing,
+  Withheld,
+  Scheduled,
+  Private,
+  Unknown,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum RequestedStatus {
+  Approved,
+  Archived,
+  Unlisted,
+  Private,
+  Draft,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum ProjectType {
+  Mod,
+  Modpack,
+  ResourcePack,
+  Shader,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum MonetizationStatus {
+  Monetized,
+  Demonetized,
+  ForceDemonetized,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct License {
+  id: String,
+  name: String,
+  url: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct GalleryImage {
+  url: String,
+  featured: bool,
+  title: Option<String>,
+  description: Option<String>,
+  created: String,
+  ordering: u32,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ModeratorMessage {
+  message: String,
+  body: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct DonationUrl {
+  id: String,
+  platform: String,
+  url: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Project {
+  id: String,
+  slug: String,
+  team: String,
+  title: String,
+  description: String,
+  categories: Vec<String>,
+  client_side: ClientSide,
+  server_side: ServerSide,
+  body: String,
+  status: Status,
+  requested_status: Option<RequestedStatus>,
+  additional_categories: Vec<String>,
+  issues_url: Option<String>,
+  source_url: Option<String>,
+  wiki_url: Option<String>,
+  discord_url: Option<String>,
+  donation_urls: Vec<DonationUrl>,
+  project_type: ProjectType,
+  downloads: u32,
+  followers: u32,
+  icon_url: Option<String>,
+  color: Option<u32>,
+  thread_id: String,
+  monetization_status: MonetizationStatus,
+  moderator_message: Option<ModeratorMessage>,
+  published: String,
+  updated: String,
+  approved: Option<String>,
+  queued: Option<String>,
+  license: Option<License>,
+  versions: Vec<String>,
+  game_versions: Vec<String>,
+  loaders: Vec<String>,
+  gallery: Vec<GalleryImage>,
+}
+
+pub async fn fetch_project(project_id: &str) -> Result<Project, reqwest::Error> {
+  let url = format!("{ENDPOINT}/project/{project_id}");
+  let client = Client::new();
+  let resp = client.get(&url).send().await?;
+  let project = resp.json::<Project>().await?;
+  Ok(project)
+}
