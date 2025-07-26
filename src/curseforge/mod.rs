19659e4b740f9c3e13e6572a6be70d524acd1f56
Sophia Bitterstar <sophia@shiningtrapezohedron.com>
2025-07-26 20:39:00 +0100

    Comment unused deps for now

--- src/curseforge/mod.rs
+++ src/curseforge/mod.rs
@@ -1,20 +1,22 @@
-use std::path::Path;
+#![allow(dead_code)]
+
+use std::path::PathBuf;
 
 #[cfg(not(windows))]
-pub(crate) fn curse_dir() -> Option<Path> { None }
+pub(crate) fn curse_dir() -> Option<PathBuf> { None }
 
 #[cfg(windows)]
-pub(crate) fn curse_dir() -> Option<&Path> {
+pub(crate) fn curse_dir() -> Option<PathBuf> {
   let install_locations = [
     "Curse",
     "Twitch",
   ];
 
-  let home_dir = directories::BaseDirs::new()?.home_dir();
+  let base_dirs: directories::BaseDirs = directories::BaseDirs::new()?;
+  let home_dir = base_dirs.home_dir();
 
   install_locations
     .iter()
     .map(|d| home_dir.join(d))
     .find(|p| p.try_exists().unwrap_or(false))
-    .map(|p| p.as_path())
 }
